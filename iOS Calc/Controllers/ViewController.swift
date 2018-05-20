79e53ea08707a25460f9fa7dcfdc7c6f70ed2165
varma-4 <varmadatla444@gmail.com>
2018-05-20 20:55:07 +0530

    Replicated iOS 11 Calculator app with Complete Auto Layout constraints allowing dynamic added on landscape mode

--- iOS Calc/Controllers/ViewController.swift
+++ iOS Calc/Controllers/ViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var equalsButton: RoundedButton!
+    @IBOutlet weak var divButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+}
 
+class RoundedButton: UIButton {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = bounds.size.height / 2
+        clipsToBounds = true
+    }
 }
 
